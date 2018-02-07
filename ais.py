674c16f0b913313fdf5a13288001cdc4ed34f1a2
birgirst <birgir.stefansson@schibsted.com>
2018-02-07 13:50:08 +0100

    Add healthcheck endpoint

--- ais.py
+++ ais.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, make_response
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/_/health')
+def health():
+    return make_response('', 200)
 
 
 def main():
