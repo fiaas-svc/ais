323e523f8ba5f456de809c830e2c936f8b01df00
Morten Lied Johansen <mortenjo@ifi.uio.no>
2018-10-22 12:58:47 +0200

    Use HTTP server from gevent

--- fiaas_ais/ais.py
+++ fiaas_ais/ais.py
@@ -1,9 +1,22 @@
+import json
+import logging
+import sys
+from datetime import datetime
+
+from gevent import monkey
+
+# This need to be before boto3
+monkey.patch_all()  # NOQA
+
+import boto3
+import time
+
 from flask import Flask, request, make_response, abort
 from flask_talisman import Talisman, DENY
-import json
-import boto3
-from datetime import datetime
+from gevent.pywsgi import WSGIServer, LoggingLogAdapter
 from prometheus_client import generate_latest, CONTENT_TYPE_LATEST, Histogram
+
+LOG = logging.getLogger(__name__)
 
 app = Flask(__name__)
 # TODO: These options are like this because we haven't set up TLS
@@ -39,6 +52,12 @@ def _verify(data):
             abort(400, '%s must be specified' % k)
 
 
+@app.route('/_/slow')
+def slow():
+    time.sleep(1)
+    return make_response('', 200)
+
+
 @app.route('/_/health')
 @health_histogram.time()
 def health():
@@ -53,8 +72,20 @@ def metrics():
     return resp
 
 
+def _init_logging():
+    root = logging.getLogger()
+    root.setLevel(logging.INFO)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(logging.Formatter("[%(asctime)s|%(levelname)7s] %(message)s [%(name)s|%(threadName)s]"))
+    root.addHandler(handler)
+
+
 def main():
-    app.run(host='0.0.0.0')
+    _init_logging()
+    log = LoggingLogAdapter(LOG, logging.DEBUG)
+    error_log = LoggingLogAdapter(LOG, logging.ERROR)
+    http_server = WSGIServer(("", 5000), app, log=log, error_log=error_log)
+    http_server.serve_forever()
 
 
 if __name__ == '__main__':
